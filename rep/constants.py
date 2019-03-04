a59c690a9e0e27f947f495506d13063f6005a349
Madalina Giurgiu <giurgiu@gagneur-desk09.i12g.informatik.tu-muenchen.de>
2019-03-04 11:12:39 +0100

    https://i12g-gagneurweb.informatik.tu-muenchen.de/gitlab/gagneurlab/rep/issues/12 compute metadata for the cross tissue matrix in oder to evaluate per tissue (i.e. compute spearman, person r^2 per tissue) #time 1h

--- rep/constants.py
+++ rep/constants.py
@@ -1,5 +1,16 @@
 
-
 class ANNDATA_CST:
     GENES = 'genes'
     SAMPLES = 'samples'
+
+class GTEX_CST:
+    INDIVIDUAL = 'Individual'
+    FROM_TISSUE = 'From_tissue'
+    TO_TISSUE = 'To_tissue'
+    FROM_PARENT_TISSUE = 'From_parent_tissue'
+    TO_PARENT_TISSUE = 'To_parent_tissue'
+    INDIV_SEQ_ASSAY = 'Indiv_Seq_Assay'
+    TISSUE = 'Tissue'
+    PARENT_TISSUE = 'Parent_Tissue'
+    GENDER = 'Gender'
+    
