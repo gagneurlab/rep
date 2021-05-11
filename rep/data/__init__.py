c8ead37bfceffbe11147bcd0f02d1ef210f0ab1e
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2021-05-11 23:20:11 +0200

    update imports

--- rep/data/__init__.py
+++ rep/data/__init__.py
@@ -6,4 +6,5 @@ from .variantdb import (
 )
 from .vep import VEPGeneLevelVariantAggregator, VEPTranscriptLevelVariantAggregator
 from .expression import GeneExpressionFetcher
-from .gene_level import REPGeneLevelDL
+from .canonical_transcript import GTExTranscriptProportions
+# from .gene_level import REPGeneLevelDL
