66aeddffec021591c23e661935af64e9f13c88f0
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2022-04-08 12:20:02 +0200

    disable variantdb imports

--- rep/data/__init__.py
+++ rep/data/__init__.py
@@ -1,11 +1,12 @@
 from . import maf
 
-from .variantdb import (
-    # GTExTranscriptProportions,
-    DesmiGTFetcher,
-    DesmiVEPAnnotation,
-)
-from .vep import VEPGeneLevelVariantAggregator, VEPTranscriptLevelVariantAggregator
-from .expression import GeneExpressionFetcher
-from .canonical_transcript import GTExTranscriptProportions
+# from .variantdb import (
+#     # GTExTranscriptProportions,
+#     DesmiGTFetcher,
+#     DesmiVEPAnnotation,
+# )
+# from .vep import VEPGeneLevelVariantAggregator, VEPTranscriptLevelVariantAggregator
+#from .expression import GeneExpressionFetcher
+#from .canonical_transcript import GTExTranscriptProportions
+from .featureset import *
 # from .gene_level import REPGeneLevelDL
