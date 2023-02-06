d986c313fc2a385b8e61e1fd6ba37ddb385ea33f
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2023-02-06 22:35:57 +0100

    implement more correct joining behavior for missing columns in feature sets

--- rep/data/featureset.py
+++ rep/data/featureset.py
@@ -1,5 +1,8 @@
 from typing import Dict, List, Union
 
+from collections import defaultdict
+from functools import reduce
+
 import pyspark
 import pyspark.sql.types as t
 import pyspark.sql.functions as f
@@ -7,6 +10,7 @@ import pyspark.sql.functions as f
 import rep.spark_functions as sf
 
 import logging
+
 log = logging.getLogger(__name__)
 
 
@@ -15,11 +19,12 @@ __all__ = [
     "join_featuresets",
 ]
 
+
 def transform_featureset(
     fset_df: pyspark.sql.dataframe.DataFrame,
     fset_name: str,
     variables: Dict[str, List[str]],
-    index_cols: List[str]
+    index_cols: List[str],
 ):
     """
     Extracts selected features from some Spark dataframe and flattens them.
@@ -34,25 +39,29 @@ def transform_featureset(
     # create column selection from variables
     feature_columns = list(sf.select_nested_fields(variables))
     # rename feature columns: add prefix
-    feature_columns = [col.alias(f"feature.{fset_name}@{alias}") for alias, col in feature_columns]
+    feature_columns = [
+        col.alias(f"feature.{fset_name}@{alias}") for alias, col in feature_columns
+    ]
 
     # keep only relevant columns from dataframe
     fset_df = fset_df.select(
         # index
         *[c for c in index_cols if c in fset_df.columns],
         # features
-        *feature_columns
+        *feature_columns,
     )
 
     return fset_df
+
 
 def join_featuresets(
     dataframes: Dict[str, pyspark.sql.dataframe.DataFrame],
     variables: Dict[str, Union[List, Dict, str]],
     index_cols: List[str],
-    fill_values: Dict[str, object]=None,
+    fill_values: Dict[str, object] = None,
     join="outer",
-    initial_df=None,
+    initial_df: pyspark.sql.dataframe.DataFrame = None,
+    ignore_missing_columns=True,
 ):
     """
     Opens multiple parquet dataset, extracts selected features and joins them to a single dataframe.
@@ -66,9 +75,15 @@ def join_featuresets(
     :param join: Type of the join.
     :param initial_df: Spark dataframe to join on all loaded feature sets.
         Especially useful for left joins.
+    :param ignore_missing_columns: Ignore if an index column is missing in all dataframes
     :returns: Spark dataframe
     """
     full_df = initial_df
+
+    # first, transform the feature sets and decide for a join order
+    partial_column_sets = defaultdict(list)
+    full_column_sets = []
+    found_columns = set()
     for (fset_name, fset_df) in dataframes.items():
         fset_variables = variables[fset_name]
 
@@ -79,22 +94,73 @@ def join_featuresets(
             index_cols=index_cols,
         )
 
-        if full_df is None:
-            full_df = fset_df
-            continue
-
         # determine join columns
-        join_columns = [c for c in index_cols if c in fset_df.columns and c in full_df.columns]
+        join_columns = frozenset({c for c in index_cols if c in fset_df.columns})
 
-        # join all dataframes
-        full_df = full_df.join(
-            fset_df,
-            on=join_columns if len(join_columns) > 0 else None,
-            how=join
+        partial_column_sets[join_columns].append(fset_df)
+
+        found_columns = found_columns.union(join_columns)
+
+    if initial_df is not None:
+        join_columns = {c for c in index_cols if c in initial_df.columns}
+        found_columns = found_columns.union(join_columns)
+
+    if set(index_cols) != found_columns:
+        if ignore_missing_columns:
+            index_cols = list(found_columns)
+        else:
+            raise ValueError(
+                f"Could not find all columns in the provided dataframes! Missing columns: '{set(index_cols).difference(found_columns)}'"
+            )
+
+    # perform partial joins
+    joint_partial_column_sets = defaultdict(list)
+    for (join_columns, fsets) in partial_column_sets.items():
+        joint_fset = reduce(
+            lambda left, right: left.join(right, on=list(join_columns), how="outer"), fsets
         )
+        joint_partial_column_sets[join_columns] = joint_fset
+
+    # compute necessary cross-join columns
+    cross_join_columns = {}
+    for join_column_set in partial_column_sets.keys():
+        cross_join_columns = {
+            *cross_join_columns,
+            *set(index_cols).difference(join_column_set),
+        }
+
+    distinct_values = dict()
+    for col in cross_join_columns:
+        # get all datasets that have the column
+        dfs = []
+        for idx, df in joint_partial_column_sets.items():
+            if col in idx:
+                # get distinct values from df
+                df_distinct = df.select(f.col(col)).distinct()
+                dfs.append(df_distinct)
+
+        distinct_values[col] = reduce(lambda left, right: left.union(right), dfs).distinct()
+
+    # now perform all cross-joins
+    joint_full_column_sets = []
+    for idx, df in joint_partial_column_sets.items():
+        df_cross_join_columns = set(index_cols).difference(idx)
+        for col in df_cross_join_columns:
+            df = df.crossJoin(distinct_values[col])
+
+        joint_full_column_sets.append(df)
+
+    # finally, join all full datasets
+    if full_df is None and join == "outer":
+        full_df = reduce(
+            lambda left, right: left.join(right, on=index_cols, how="outer"),
+            joint_full_column_sets,
+        )
+    else:
+        for other_df in joint_full_column_sets:
+            full_df = full_df.join(other_df, on=index_cols, how=join)
 
     if fill_values is not None:
         full_df = full_df.fillna(fill_values)
 
     return full_df
-
--- rep/polars_functions/featureset.py
+++ rep/polars_functions/featureset.py
@@ -1,11 +1,15 @@
 from typing import Dict, List, Union
 
+from collections import defaultdict
+from functools import reduce
+
 import polars as pl
 import polars.datatypes as t
 
 from .reshape import select_nested_fields
 
 import logging
+
 log = logging.getLogger(__name__)
 
 
@@ -19,7 +23,7 @@ def transform_featureset(
     fset_df: Union[pl.DataFrame, pl.LazyFrame],
     fset_name: str,
     variables: Dict[str, List[str]],
-    index_cols: List[str]
+    index_cols: List[str],
 ):
     """
     Extracts selected features from some Spark dataframe and flattens them.
@@ -34,15 +38,19 @@ def transform_featureset(
     # create column selection from variables
     feature_columns = list(select_nested_fields(variables))
     # rename feature columns: add prefix
-    feature_columns = [col.alias(f"feature.{fset_name}@{alias}") for alias, col in feature_columns]
+    feature_columns = [
+        col.alias(f"feature.{fset_name}@{alias}") for alias, col in feature_columns
+    ]
 
     # keep only relevant columns from dataframe
-    fset_df = fset_df.select([
-        # index
-        *[c for c in index_cols if c in fset_df.columns],
-        # features
-        *feature_columns
-    ])
+    fset_df = fset_df.select(
+        [
+            # index
+            *[c for c in index_cols if c in fset_df.columns],
+            # features
+            *feature_columns,
+        ]
+    )
 
     return fset_df
 
@@ -51,9 +59,10 @@ def join_featuresets(
     dataframes: Dict[str, Union[pl.DataFrame, pl.LazyFrame]],
     variables: Dict[str, Union[List, Dict, str]],
     index_cols: List[str],
-    fill_values: Dict[str, object]=None,
+    fill_values: Dict[str, object] = None,
     join="outer",
-    initial_df: Union[pl.DataFrame, pl.LazyFrame]=None,
+    initial_df: Union[pl.DataFrame, pl.LazyFrame] = None,
+    ignore_missing_columns=True,
 ):
     """
     Opens multiple parquet dataset, extracts selected features and joins them to a single dataframe.
@@ -67,9 +76,15 @@ def join_featuresets(
     :param join: Type of the join.
     :param initial_df: dataframe to join on all loaded feature sets.
         Especially useful for left joins.
+    :param ignore_missing_columns: Ignore if an index column is missing in all dataframes
     :returns: Polars dataframe
     """
     full_df = initial_df
+
+    # first, transform the feature sets and decide for a join order
+    partial_column_sets = defaultdict(list)
+    full_column_sets = []
+    found_columns = set()
     for (fset_name, fset_df) in dataframes.items():
         fset_variables = variables[fset_name]
 
@@ -80,19 +95,71 @@ def join_featuresets(
             index_cols=index_cols,
         )
 
-        if full_df is None:
-            full_df = fset_df
-            continue
-
         # determine join columns
-        join_columns = [c for c in index_cols if c in fset_df.columns and c in full_df.columns]
+        join_columns = frozenset({c for c in index_cols if c in fset_df.columns})
 
-        # join all dataframes
-        full_df = full_df.join(
-            fset_df,
-            on=join_columns if len(join_columns) > 0 else None,
-            how=join
+        partial_column_sets[join_columns].append(fset_df)
+
+        found_columns = found_columns.union(join_columns)
+
+    if initial_df is not None:
+        join_columns = {c for c in index_cols if c in initial_df.columns}
+        found_columns = found_columns.union(join_columns)
+
+    if set(index_cols) != found_columns:
+        if ignore_missing_columns:
+            index_cols = list(found_columns)
+        else:
+            raise ValueError(
+                f"Could not find all columns in the provided dataframes! Missing columns: '{set(index_cols).difference(found_columns)}'"
+            )
+
+    # perform partial joins
+    joint_partial_column_sets = defaultdict(list)
+    for (join_columns, fsets) in partial_column_sets.items():
+        joint_fset = reduce(
+            lambda left, right: left.join(right, on=list(join_columns), how="outer"), fsets
         )
+        joint_partial_column_sets[join_columns] = joint_fset
+
+    # compute necessary cross-join columns
+    cross_join_columns = {}
+    for join_column_set in partial_column_sets.keys():
+        cross_join_columns = {
+            *cross_join_columns,
+            *set(index_cols).difference(join_column_set),
+        }
+
+    distinct_values = dict()
+    for col in cross_join_columns:
+        # get all datasets that have the column
+        dfs = []
+        for idx, df in joint_partial_column_sets.items():
+            if col in idx:
+                # get distinct values from df
+                df_distinct = df.select(pl.col(col)).unique()
+                dfs.append(df_distinct)
+
+        distinct_values[col] = pl.concat(dfs).unique()
+
+    # now perform all cross-joins
+    joint_full_column_sets = []
+    for idx, df in joint_partial_column_sets.items():
+        df_cross_join_columns = set(index_cols).difference(idx)
+        for col in df_cross_join_columns:
+            df = df.join(distinct_values[col], how="cross")
+
+        joint_full_column_sets.append(df)
+
+    # finally, join all full datasets
+    if full_df is None and join == "outer":
+        full_df = reduce(
+            lambda left, right: left.join(right, on=index_cols, how="outer"),
+            joint_full_column_sets,
+        )
+    else:
+        for other_df in joint_full_column_sets:
+            full_df = full_df.join(other_df, on=index_cols, how=join)
 
     if fill_values is not None:
         fill_exprs = []
@@ -101,11 +168,9 @@ def join_featuresets(
             if full_df.schema[col] in t.FLOAT_DTYPES:
                 expr = expr.fill_nan(fval)
             expr = expr.alias(col)
-            
+
             fill_exprs.append(expr)
-        
+
         full_df = full_df.with_columns(fill_exprs)
 
-
     return full_df
-
